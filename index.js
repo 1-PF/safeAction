b3454c7153fbaed9e8f6159a0cdae7b735c0208a
alexandrsokolov <alexandr.sokolov@1pf.cz>
2021-05-20 16:57:11 +0200

    V5 read File Check

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const fs = require('fs')
 try {
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}`);
+    fs.readFile('.github/workflows/maven.yml','utf8', function(err, data){
+        if(err){
+            return console.log(err)
+        }
+        console.log(data)
+    })
     const time = (new Date()).toTimeString()
     core.setOutput("time",time)
     const payload = JSON.stringify(github.context.payload, undefined , 2)
