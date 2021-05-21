8ad09b147080df8b3774cf648ee50bad69adcf8c
alexandrsokolov <alexandr.sokolov@1pf.cz>
2021-05-21 09:39:41 +0200

    Changed index

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const fs = require('fs')
 try {
-    fs.readFile('.github/workflows/maven.yml', function(err, data){
+    fs.readFile('.github/workflows/application.yml','utf-8', function(err, data){
         if(err){
             return console.log(err)
         }
         console.log(data)
-        core.setOutput("file",data)
+        core.setOutput("file", data)
     }) 
 } catch(error) {
     core.setFailed(error.message)
