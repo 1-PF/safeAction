370647ee4f903127a143bafd5fffe972328ea735
alexandrsokolov <alexandr.sokolov@1pf.cz>
2021-05-21 09:21:08 +0200

    Using decoder

--- index.js
+++ index.js
@@ -2,22 +2,12 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const fs = require('fs')
 try {
-    fs.readFile('.github/workflows/maven.yml','utf8', function(err, data){
+    fs.readFile('.github/workflows/maven.yml','utf-8', function(err, data){
         if(err){
             return console.log(err)
         }
         console.log(data)
         core.setOutput("file",data)
-
-
-        let decoder = new TextDecoder();
-        let promise = OS.File.read(".github/workflows/maven.yml")
-        promise = promise.then(
-            function onSuccess(array) {
-              return decoder.decode(array);        // Convert this array to a text
-            }
-          );
-        console.log(promise)
     }) 
 } catch(error) {
     core.setFailed(error.message)
