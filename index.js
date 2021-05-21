a3d96426b00073fd29b60225af23db9b5a9c9d5c
alexandrsokolov <alexandr.sokolov@1pf.cz>
2021-05-21 14:27:01 +0200

    List of safety actions

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 const fs = require('fs')
+
+const safeActions = [
+    'cache',
+    'actions'
+]
 try {
     fs.readdir('../../_actions/actions', function(err, data){
         if(err){
             return console.log(err)
         }
-        console.log(data)
+        //console.log(data)
+        data.forEach(action => {
+            if(!safeActions.includes(action)){
+                throw "Not safety action was detected"
+            }
+        })
         core.setOutput("directoryFiles", data)
     }) 
 } catch(error) {
