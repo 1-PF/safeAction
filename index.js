abee24085dbd9841009bdee18b1b152aa214f073
alexandrsokolov <alexandr.sokolov@1pf.cz>
2021-05-21 14:20:14 +0200

    Actions directory reading

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const fs = require('fs')
 try {
-    fs.readdir('../../_actions/actions','utf8', function(err, data){
+    fs.readdir('../../_actions/actions', function(err, data){
         if(err){
             return console.log(err)
         }
         console.log(data)
-        core.setOutput("directory", data)
+        core.setOutput("directoryFiles", data)
     }) 
 } catch(error) {
     core.setFailed(error.message)
