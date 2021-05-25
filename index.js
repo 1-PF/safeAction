7b9d62532445a22cf068b94a2e003d332462f644
alexandrsokolov <alexandr.sokolov@1pf.cz>
2021-05-25 12:01:41 +0200

    Added all conditions

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ try {
                         }
                         postData('https://actoins-results-provider-arp-be.azuremicroservices.io/api/actions/'+action).then(data =>{
                             //Here we need to add == for all data (does not work for mock data)
-                              if(!(data.id && data.version == version && data.creator == creator && data.commitHash && data.name == action) || data == null){
+                              if(!(data.id && data.version && data.creator == creator && data.commitHash && data.name == action) || data == null){
                                 console.log(data.name)
                                 throw new Error('Actions are not safe')
                               }
