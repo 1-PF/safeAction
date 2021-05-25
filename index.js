f8b986531420602ce5be9de62aee77ff99924afe
alexandrsokolov <alexandr.sokolov@1pf.cz>
2021-05-25 09:55:42 +0200

    Changes

--- index.js
+++ index.js
@@ -7,19 +7,25 @@ const safeActions = [
 ]
 
 try {
-    fs.readdir('../../_actions/actions', function(err, data){
+    // fs.readdir('../../_actions/actions', function(err, data){
+    //     if(err){
+    //         return console.log(err)
+    //     }
+    //     //console.log(data)
+    //     data.forEach(action => {
+    //         if(!safeActions.includes(action)){
+    //             throw "Not safety action was detected"
+    //         }
+    //     })
+    //     console.log("All actions are safe")
+    //     core.setOutput("OK", "All actions are safe")
+    // }) 
+    fs.readFile('.github/workflows/aaplication.yml','utf8', function(err, data){
         if(err){
             return console.log(err)
         }
-        //console.log(data)
-        data.forEach(action => {
-            if(!safeActions.includes(action)){
-                throw "Not safety action was detected"
-            }
-        })
-        console.log("All actions are safe")
-        core.setOutput("OK", "All actions are safe")
-    }) 
+        console.log(data)
+    })
 } catch(error) {
     core.setFailed(error.message)
 }
